f6e09382f78f77dd6b2332b552ee0ad3f96efb49
Geoff Stockham <geoff@stormid.com>
2016-12-16 10:24:22 +0000

    Add mapping test

---
+++ test/Storm.InterviewTest.Hearthstone.Tests/Mapping/MappingFixture.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Moq;
+using Storm.InterviewTest.Hearthstone.Core.Domain;
+using Storm.InterviewTest.Hearthstone.Core.Mapping;
+using Storm.InterviewTest.Hearthstone.Core.Queries;
+using Storm.InterviewTest.Hearthstone.Core.Services;
+
+namespace Storm.InterviewTest.Hearthstone.Tests.Mapping
+{
+    public class MappingFixture
+    {
+        public HeroCard HeroCard { get; private set; }
+        public IMapper Mapper { get; private set; }
+        public Mock<IHearthstoneCardCache> Repository { get; private set; }
+
+
+        public MappingFixture()
+        {
+            HeroCard = new HeroCard("H1")
+            {
+                Name = "My Hero",
+                PlayerClass = null
+            };
+
+            Repository = new Mock<IHearthstoneCardCache>();
+
+            // Mock our repo to only return hero card whenever player class is not null
+            Repository.Setup(s => s.Query(It.Is<FindHeroCardQuery>(y => y != null))).Returns(HeroCard);
+
+            Mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CardsMappingProfile>();
+
+            }).CreateMapper(t =>
+            {
+                // Poor man's dependency injection
+                if (t == typeof(HeroResolver))
+                    return new HeroResolver(Repository.Object);
+                return null;
+            });
+        }
+    }
+}
---
+++ test/Storm.InterviewTest.Hearthstone.Tests/Mapping/WhenMappingHeroCard.cs
@@ -0,0 +1,41 @@
+﻿using Shouldly;
+using Storm.InterviewTest.Hearthstone.Core.Domain;
+using Storm.InterviewTest.Hearthstone.Models;
+using Xunit;
+
+namespace Storm.InterviewTest.Hearthstone.Tests.Mapping
+{
+    public class WhenMappingHeroCard : IClassFixture<MappingFixture>
+    {
+        public MappingFixture Fixture { get; }
+        private HeroCard _card;
+
+        public WhenMappingHeroCard(MappingFixture fixture)
+        {
+            Fixture = fixture;
+            _card = new HeroCard("M1")
+            {
+                PlayerClass = "Priest",
+                Name = "Anduin",
+                Faction = FactionTypeOptions.Neutral,
+                Rarity = RarityTypeOptions.Legendary,
+                Attack = 0,
+                Cost = 0,
+                Health = 30
+            };
+        }
+
+        [Fact]
+        public void ShouldMapHeroModelCorrectly()
+        {
+            var result = Fixture.Mapper.Map<HeroCard, HeroModel>(_card);
+
+            result.Name.ShouldBe(_card.Name);
+            result.ImageUrl.ShouldBe(_card.ImageUrl.ToString());
+            result.PlayerClass.ShouldBeNull();
+            result.PlayerClass.ShouldBeNull();
+            result.Health.ShouldBe(_card.Health);
+            result.Attack.ShouldBe(_card.Attack);
+        }
+    }
+}
